d87b1dbd889305e137cc4cbc3b30cfee66d73059
lillaball-pvcc <157185595+lillaball-pvcc@users.noreply.github.com>
2024-03-11 19:40:35 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# Name: Lilla Ball
+# Prog Purpose: Hello World
+
+print("Hello world! This is my first Python Program.")
+print("I can program in HTML, which is awesome.")
+print("However, HTML can not do arithmetic.")
+print("But python can: \n\t Add \n\t Subtract \n\t Multiply \n\t Divide")
+print("and lots of other things!")
